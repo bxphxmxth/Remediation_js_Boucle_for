3b3cc8f0cdf741dfa8617eac744097e21a19a21a
bxphxmxth <fushs@hotmail.fr>
2021-05-11 18:54:21 +0200

    AMEN EXO3 UN TRUC MARCHE è__é

--- src/js/main.js
+++ src/js/main.js
@@ -31,22 +31,21 @@
 
 // for (let i = 0; i < tab2.length; i++){
 //     if (tab2[i].charAt(0) == "A"){
-//         console.log("Yo" + " " + tab2[i] +);
+//         console.log("Yo" + " " + tab2[i]);
 //     }else if (tab2[i].charAt(0) == "E"){
-//         console.log("Salut" + " " + tab2[i] +);
+//         console.log("Salut" + " " + tab2[i]);
 
 //     }else{
 //         console.log("Ca gaze" + " " + tab2[i] + " ?");
 
 //     }
-
-    
 // }
 
 
 
 
 
+// TEST FAUX
 // for (let i = 0; i < tab2.length; i++) {
 //     console.log("Yo " + " " + tab2[i].charAt());
 //     console.log("Salut " + " " + tab2[i].charAt());
@@ -61,23 +60,55 @@
 // 3.
 // Trouvez un moyen de mettre en majuscule un élément sur deux dans un array avec une boucle for (ex: ["JAVASCRIPT", "c'est", "FACILE"])
 
+let tab = ["Wesh ", "ca va ", "ou ", "quoi?"];
+
+// Test 1 
+// for (let i = 0; i < tab.length; i++){
+
+//     console.log(tab[i].toUpperCase());
+// }
+
+//even odd
+
+//Test 2
+// for (let i = 0; i < tab.length; i++){
+//     tab[i].toUpperCase();
+//     console.log(tab[i]);
+// }
+
+
+// test 3 IF ELSE
+
+for (let i = 0; i < tab.length; i++){
+    if (tab[i] == tab[1]){
+        console.log(tab[i].toUpperCase());
+
+    }else if (tab[i] == tab[1]){
+        console.log(tab[i].toUpperCase());
+
+    }else{
+        console.log(tab[i].toLowerCase());
+}
+
+
+
 
 
 // 4.
 // // Trouvez un moyen de faire rentrer un mot de passe a un utilisateur avec une boucle for et des conditions. S'il ne trouve pas le mot de passe après 3 essais, affichez-lui "accès refusé", sinon, affichez "accès accordé".
 
 
-let mdp = prompt("Entrez un mdp");
+// let mdp = prompt("Entrez un mdp");
 
 
-for (let i = 0; i < 3; i++) {
-   if ( mdp != "123"){
-       alert("Accès refusé, réessaye");
-       let mdp = prompt("Entrez un mdp");
+// for (let i = 0; i < 3; i++) {
+//    if ( mdp != "123"){
+//        alert("Accès refusé, réessaye");
+//        let mdp = prompt("Entrez un mdp");
 
-   }else{
-       alert("Welcomeee");
-   }
-}
+//    }else{
+//        alert("Welcomeee");
+//    }
+// }
 
 
